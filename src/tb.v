48bfda718a2874b5a54271942412b73f675cd439
ridger <ridger@live.cn>
2023-11-03 12:25:28 +0800

    update tb.v

--- src/tb.v
+++ src/tb.v
@@ -28,7 +28,7 @@ module tb ();
     wire [7:0] uio_out;
     wire [7:0] uio_oe;
 
-    tt_um_rnn tt_um_seven_rnn (
+    tt_um_rnn tt_um_rnn (
     // include power ports for the Gate Level test
     `ifdef GL_TEST
         .VPWR( 1'b1),
