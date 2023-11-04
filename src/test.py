d191af74ab9f2df436837a02980b85a83be19cd3
ridger <ridger@live.cn>
2023-11-04 12:52:03 +0800

    update test

--- src/test.py
+++ src/test.py
@@ -4,4 +4,21 @@ from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
 @cocotb.test()
 async def test_my_design(dut):
+
+    CONSTANT_CURRENT = 40
     dut._log.info("start")
+
+    clock = Clock(dut.clk, 1, units='ns')
+    cocotb.start_soon(clock.start())
+
+    dut.rst_n.value = 0 #low to reset
+    await ClockCycles(dut.clk, 10)
+    dut.rst_n.value = 1 #reset now!
+
+    dut.ui_in.value = CONSTANT_CURRENT
+    dut.ena.value = 1 #enable design
+
+    for _ in range(100):
+        await RisingEdge(dut.clk)
+
+    dut._log.info("finish")
